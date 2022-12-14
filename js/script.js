9bad88c18d95841bc6287310c34f533ae4e3123c
RiccardoNestola <riccardinux@gmail.com>
2022-12-14 17:20:42 +0100

    create array and layout

--- js/script.js
+++ js/script.js
@@ -3,7 +3,39 @@ const { createApp } = Vue
     createApp({
         data() {
         return {
-            message: 'Hello Vue!'
+            activeIndex : 0,
+            todoList: [
+
+                {
+                    text:"Andare a fare la prima spesa ",
+                    done: true
+                },
+                {
+                    text:"Andare a fare la seconda spesa",
+                    done: false
+                },
+                {
+                    text:"studiare",
+                    done: true
+                },
+                {
+                    text:"uscire",
+                    done: true
+                },
+                {
+                    text:"suonare",
+                    done: true
+                },
+                {
+                    text:"andare al cinema",
+                    done: true
+                },
+                {
+                    text:"pagare le tasse",
+                    done: true
+                },
+            ],
         }
         }
     }).mount('#app')
+
